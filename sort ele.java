1afca440c466203d49fbbdea55a5f6ba6878531d
madhav9634 <madhavsharma963420@gmail.com>
2026-02-18 09:33:33 +0530

    Sort element

---
+++ sort ele.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args){
+        int arr[] = {20,5,25,6,40};
+        int n = 5;
+        int temp;
+
+        for(int i = 0; i<n-1;i++){
+            for(int j = i+1; j<n;j++){
+                if(arr[i]>arr[j]){
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted array");
+        for(int i = 0;i<n;i++){
+            // System.out.println(arr[i]);
+            System.out.println("P" + (i+1) + " = " + arr[i]);
+        }
+    }
+}
